c79f5ae4be8708791cb7ec7287979ad70de7fc11
fateme-soleymani <fateme_soleymanid@yahoo.com>
2020-11-19 16:32:26 +0330

    with check date

--- task.py
+++ task.py
@@ -1,32 +1,59 @@
-import datetime
+from datetime import datetime
+from tkinter.messagebox import showinfo
 
 
-class Task(object):
-    def __init__(self, description, link, picture_path, date, time, priority):
+class Task:
+    def __init__(self,description,link,loc,year,month,day,hour,minute):
         self.description = description
         self.link = link
-        self.picture_path = picture_path
-        self.date = date
-        self.time = time
-        self.priority = priority
+        self.loc = loc
+        self.year= year
+        self.month= month
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        # self.priority = priority
 
-    def __del__(self):
-        pass
+    def remind(self):
+            if self.year == datetime.now().year \
+                    and self.month == datetime.now().month \
+                    and self.day == datetime.now().day \
+                    and self.hour == datetime.now().hour \
+                    and self.minute == datetime.now().minute:
+                return True
+            return False
 
-    def edit(self):
-        pass
+class Check:
+    def __init__(self,year,month,day,hour,minute):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
+        self.minute = minute
 
-    def snooze(self):
-        pass
-
-    def dismiss(self):
-        pass
-
-
-class SubTask(Task):
-    def __init__(self):
-        super.__init__()
-
-
-class Group(object):
-    pass
+    def check(self):
+        if self.year == 0 \
+                or self.month == 0 \
+                or self.day == 0:
+            showinfo("notification", "Enter date and time!!")
+            return False
+        elif self.year == datetime.now().year:
+            if self.month < datetime.now().month:
+                showinfo("notification", "month pass")
+                return False
+            elif self.month == datetime.now().month:
+                if self.day < datetime.now().day:
+                    showinfo("notification", "day pass")
+                    return False
+                elif self.day == datetime.now().day:
+                    if self.hour < datetime.now().hour:
+                        showinfo("notification", "hour pass")
+                        return False
+                    elif self.hour == datetime.now().hour:
+                        if self.minute < datetime.now().minute:
+                            showinfo("notification", "minute pass")
+                            return False
+                        else:
+                            return True
+        else:
+            return True
