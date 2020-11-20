97eaf6755ca609e7ef3023f27763ad5f271f1061
fateme-soleymani <fateme_soleymanid@yahoo.com>
2020-11-20 11:26:05 +0330

    finally

--- main.py
+++ main.py
@@ -7,8 +7,26 @@ from tkinter import *
 from tkinter.ttk import *
 from task import *
 
+
 def new():
-    exit()
+    t.destroy()
+
+    def edit():
+        li = link.get()
+        loc = location.get()
+        des = descrip.get(1.0, END)
+        y = year.get()
+        m = month.get()
+        d = day.get()
+        h = hour.get()
+        min = minute.get()
+        p = priority.get()
+        file = file_name.get()
+        cat = category.get()
+        sub=subtask.get()
+        obj_task = Task(file,sub, cat, des, li, loc, y, m, d, h, min)
+        obj_task.edit()
+
     def main():
         li = link.get()
         loc = location.get()
@@ -21,88 +39,77 @@ def new():
         p = priority.get()
         file = file_name.get()
         cat = category.get()
-        obj_check = Check(y, m, d, h, min)
-        if obj_check.check():
-            file_cat = open(cat + '.txt', 'a')
-            file_cat.write(file)
-            file_cat.close()
-            f = open(file + '.txt', 'w')
-            f.write('{}\n{}\n{}\n{}\\{}\\{}\n{}:{}'.format(des, li, loc, str(y), str(m), str(d), str(h), str(min)))
-            f.close()
-            showinfo("notification", "reminder has been set")
-            obj_task = Task(des, li, loc, y, m, d, h, min)
+        obj_task = Task(file, cat, des, li, loc, y, m, d, h, min)
+
+        if obj_task.check():
+            obj_task.file_write()
             flag = True
             while flag == True:
+                t2.destroy()
                 if obj_task.remind():
-                    screen = Tk()
-                    Label(screen, text="Description :").grid(row=0, column=0, pady=10)
-                    Label(screen, text=des).grid(row=0, column=1)
-                    Label(screen, text="Link :").grid(row=1, column=0, pady=10)
-                    Label(screen, text=li).grid(row=1, column=1)
-                    Label(screen, text="location :").grid(row=2, column=0, pady=10)
-                    Label(screen, text=loc).grid(row=2, column=1)
                     playsound("ring.mp3")
                     flag = False
-                    screen.mainloop()
+                    print(obj_task)
 
-    t = Tk()
-    t.geometry("600x350")
-    t.title("REMINDER")
-    Label(t, text="Name").grid(row=0)
-    file_name = Entry(t)
+    t2 = Tk()
+    t2.geometry("600x350")
+    t2.title("REMINDER")
+    Label(t2, text="Name").grid(row=0)
+    file_name = Entry(t2)
     file_name.grid(row=0, column=1, pady=10)
-    Label(t, text="Description :").grid(row=1, column=0, pady=10)
+    Label(t2, text="Description :").grid(row=1, column=0, pady=10)
 
-    descrip = Text(t, width=50, height=2, font=(("Arial"), 10), wrap=WORD)
+    descrip = Text(t2, width=50, height=2, font=(("Arial"), 10), wrap=WORD)
     descrip.grid(row=1, column=1, columnspan=3, sticky=W)
 
-    Label(t, text="Link :").grid(row=2, column=0, pady=10)
-    link = Entry(t)
+    Label(t2, text="Link :").grid(row=2, column=0, pady=10)
+    link = Entry(t2)
     link.grid(row=2, column=1, sticky=W)
 
-    Label(t, text="location :").grid(row=3, column=0, pady=10)
-    location = Entry(t)
+    Label(t2, text="location :").grid(row=3, column=0, pady=10)
+    location = Entry(t2)
     location.grid(row=3, column=1, sticky=W)
 
-    Label(t, text="Priority :").grid(row=4, column=0, pady=10)
-    priority = Combobox(t, values=["important", "normal"], state='readonly')
+    Label(t2, text="Priority :").grid(row=4, column=0, pady=10)
+    priority = Combobox(t2, values=["important", "normal"], state='readonly')
     priority.grid(row=4, column=1, sticky=W)
 
-    Label(t, text="Date(y/m/d) :").grid(row=5, column=0, pady=10)
+    Label(t2, text="Date(y/m/d) :").grid(row=5, column=0, pady=10)
     year = IntVar()
-    ttk.Combobox(t, textvariable=year,
+    ttk.Combobox(t2, textvariable=year,
                  values=list(range(2020, 2031)),
                  state='readonly').grid(row=5, column=1, sticky=W)
     month = IntVar()
-    ttk.Combobox(t, textvariable=month,
+    ttk.Combobox(t2, textvariable=month,
                  values=list(range(1, 13)),
                  state='readonly').grid(row=5, column=2, sticky=W)
     day = IntVar()
-    ttk.Combobox(t, textvariable=day,
+    ttk.Combobox(t2, textvariable=day,
                  values=list(range(1, 32)),
                  state='readonly').grid(row=5, column=3, sticky=W)
 
-    Label(t, text="Time(h:m) :").grid(row=6, column=0)
+    Label(t2, text="Time(h:m) :").grid(row=6, column=0)
     hour = IntVar()
-    ttk.Combobox(t, textvariable=hour,
+    ttk.Combobox(t2, textvariable=hour,
                  values=list(range(0, 24)),
                  state='readonly').grid(row=6, column=1, pady=10, sticky=W)
     minute = IntVar()
-    ttk.Combobox(t, textvariable=minute,
+    ttk.Combobox(t2, textvariable=minute,
                  values=list(range(0, 60)),
                  state='readonly').grid(row=6, column=2, sticky=W)
-    Label(t, text="Category").grid(row=7, column=0)
-    category = ttk.Combobox(t, values=['maktab', 'uni', 'home'])
+    Label(t2, text="Category").grid(row=7, column=0)
+    category = ttk.Combobox(t2, values=['maktab', 'uni', 'home'])
     category.grid(row=7, column=1)
+    Label(text="Subtask").grid(row=7,column=2,sticky=E)
+    subtask=Entry(t2)
+    subtask.grid(row=7,column=3)
+    Button(t2, text="set", command=main, width=15, bg="grey").grid(row=8, column=1, pady=20)
+    t2.mainloop()
 
-    set_button = Button(t, text="set", command=main).grid(row=8, column=3, pady=20, sticky=W)
-    set_button = Button(t, text="subtask").grid(row=8, column=2, pady=20, sticky=E)
 
-    t.mainloop()
-
-
-def exit():
+def exit_m():
     t.destroy()
+
 
 def edit():
     import tkinter as tk
@@ -153,11 +160,19 @@ def edit():
     window.mainloop()
 
 
+def remove():
+    # name_file.get open and delete
+    showinfo("notifacation", "your task delete")
+    pass
+
+
 t = Tk()
-t.geometry("300x300")
 t.title("Menu")
-edit_button = Button(t, text="edit", command=edit).grid(row=1, column=0)
-remove_button = Button(t, text="remove").grid(row=2, column=0)
-new_button = Button(t, text="new", command=new).grid(row=3, column=3)
-exit_button = Button(t, text="exit", command=exit).grid(row=3, column=1)
+Label(text="name of task").grid(row=0, column=0)
+name_file = Entry(t)
+name_file.grid(row=0, column=1)
+edit_button = Button(t, text="edit", command=edit, width=15, bg="grey").grid(row=2, column=2)
+remove_button = Button(t, text="remove", width=15, command=remove, bg="grey").grid(row=2, column=3)
+new_button = Button(t, text="new", command=new, width=15, bg="grey").grid(row=3, column=2, pady=10)
+exit_button = Button(t, text="exit", command=exit_m, width=15, bg="grey").grid(row=3, column=3, padx=10)
 t.mainloop()
